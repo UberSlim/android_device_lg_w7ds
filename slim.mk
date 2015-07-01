51159b3cc60c91150cc685dee5650bce7cd80311
ROM-Jeremy <acasher754@mail.cfcc.edu>
2015-06-30 21:09:47 -0400

    Update and rename cm.mk to slim.mk

---
+++ slim.mk
@@ -0,0 +1,12 @@
+# Inherit some common Slim stuff.
+$(call inherit-product, vendor/slim/config/common_full_phone.mk)
+
+# Boot animation
+TARGET_SCREEN_WIDTH := 540
+TARGET_SCREEN_HEIGHT := 960
+
+# Release name
+PRODUCT_RELEASE_NAME := LG L90
+PRODUCT_NAME := slim_w7
+
+$(call inherit-product, device/lge/w7/full_w7.mk)
